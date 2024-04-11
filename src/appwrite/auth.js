f685a732c4cbc4bb8d3f0e6e7434fc59c77252c5
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-11 19:10:10 +0530

    Fix typo in createAccount function parameter name in auth.js

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -9,7 +9,7 @@ export class AuthService {
   .setProject(conf.appwriteProjectId)
   this.account = new Account(this.client)
  }
- async createAccount({email, password,name}) {
+ async createAccount({email, password, name}) {
     try{
     const userAccount = this.account.create(ID.unique(), email, password, name);
     if(userAccount) {
