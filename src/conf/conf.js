b4bc715bf14a9cf86df7e085c33ddfdea8bb855c
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-11 17:12:49 +0530

    conf.js created

---
+++ src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf ={}
+appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL)
+appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID)
+appwriteUDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_IDL)
+appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
+appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+
+export default conf
