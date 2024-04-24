271da5af4c85b6b70062787ea9bcfc48d843b78f
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-24 22:32:32 +0530

    change

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,5 +4,4 @@ appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID)
 appwriteUDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_IDL)
 appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
 appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-
 export default conf
